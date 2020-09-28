88117026daf43b7dc64f2d94a405aae29ca24977
rohitm53 <36997587+rohitm53@users.noreply.github.com>
2020-09-29 00:32:08 +0530

    dd

--- app/src/main/java/com/smartherd/multiplescreensupport/MainActivity.kt
+++ app/src/main/java/com/smartherd/multiplescreensupport/MainActivity.kt
@@ -1,32 +1,17 @@
 package com.smartherd.multiplescreensupport
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MyCommunicator {
-
-	private var mIsDualPane = false
+class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-        val fragmentBView = findViewById<View>(R.id.fragmentB)
-        mIsDualPane = fragmentBView?.visibility == View.VISIBLE
+		iv2.setImageResource(R.drawable.ic_board)
+
 	}
 
-	override fun displayDetails(title: String, description: String) {
-
-        if (mIsDualPane) { // If we are in Tablet
-            val fragmentB = supportFragmentManager.findFragmentById(R.id.fragmentB) as FragmentB?
-            fragmentB?.displayDetails(title, description)
-        } else { // When we are in Smart phone
-            val intent = Intent(this, DetailActivity::class.java)
-            intent.putExtra("title", title)
-            intent.putExtra("description", description)
-            startActivity(intent)
-        }
-	}
 }
